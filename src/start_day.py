f51b6d39f03b6c410b153217d542987281342939
Moses Wynn <accounts@moseswynn.com>
2025-12-03 16:19:30 -0500

    add `start-day` boilerplate script

---
+++ src/start_day.py
@@ -0,0 +1,38 @@
+import os
+import tomlkit
+
+def main():
+    solution_text = """
+from base_solution import BaseSolution
+import os
+
+class Solution(BaseSolution):
+    def __init__(self, input):
+        super().__init__(input)
+        
+    def solve_part1(self):
+        pass
+    
+    def solve_part2(self):
+        pass
+
+def main():
+    input_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    s = Solution(input_path)
+    print(f"Part 1 Solution: {s.solve_part1()}")
+    print(f"Part 2 Solution: {s.solve_part2()}")
+    """
+    current_dir = os.path.dirname(__file__)
+    days = [int(n[-1]) for n in os.listdir(current_dir) if n.startswith("day")]
+    next_day = f"day{max(days)+1}"
+    next_day_path = os.path.abspath(os.path.join(current_dir, next_day))
+    os.mkdir(next_day_path)
+    solution_path = os.path.abspath(os.path.join(next_day_path, "solution.py"))
+    with open(solution_path, "w+") as f:
+        f.write(solution_text)
+    with open("pyproject.toml") as f:
+        doc = tomlkit.parse(f.read())
+    
+    doc["project"]["scripts"][next_day] = f"{next_day}.solution:main"
+    with open("pyproject.toml", "w+") as f:
+        f.write(tomlkit.dumps(doc))
